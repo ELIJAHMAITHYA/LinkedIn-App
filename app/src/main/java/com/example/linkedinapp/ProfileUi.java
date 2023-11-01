cb9bf36f17c9c5362e8c4a0f09364292260e526e
Sila Elijah <elimaithya21@gmail.com>
2023-11-01 15:02:40 +0300

    Start Screen, Login, SignUp UI

---
+++ app/src/main/java/com/example/linkedinapp/ProfileUi.java
@@ -0,0 +1,14 @@
+package com.example.linkedinapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileUi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_ui);
+    }
+}
---
+++ app/src/main/java/com/example/linkedinapp/signUp.java
@@ -0,0 +1,13 @@
+package com.example.linkedinapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class signUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
