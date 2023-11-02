f6b9bec970c45fb8d2a538befb5957b557be734c
Sila Elijah <elimaithya21@gmail.com>
2023-11-02 10:42:37 +0300

    Connecting with dataBase

--- app/src/main/java/com/example/linkedinapp/login.java
+++ app/src/main/java/com/example/linkedinapp/login.java
@@ -1,14 +1,77 @@
 package com.example.linkedinapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class login extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+    private EditText loginEmail, loginPassword;
+    private TextView signupText;
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        auth = FirebaseAuth.getInstance();
+        loginEmail = findViewById(R.id.login_email);
+        loginPassword = findViewById(R.id.login_password);
+        signupText = findViewById(R.id.signUpText);
+        loginButton = findViewById(R.id.login_button);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = loginEmail.getText().toString();
+                String password = loginPassword.getText().toString();
+
+                if (!email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    if (!password.isEmpty()) {
+                        auth.signInWithEmailAndPassword(email, password)
+                                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                                    @Override
+                                    public void onSuccess(AuthResult authResult) {
+                                        Toast.makeText(login.this, "Login Succesful", Toast.LENGTH_LONG).show();
+                                        startActivity(new Intent(login.this, ProfileUi.class));
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(login.this, "Login Failed", Toast.LENGTH_LONG).show();
+                                    }
+                                });
+                    } else {
+                        loginPassword.setError("Password Cannot be empty");
+                    }
+                } else if (email.isEmpty()) {
+                    loginEmail.setError("Email cannot be empty");
+                } else {
+                    loginEmail.setError("Please enter valid Details");
+                }
+            }
+        });
+        signupText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(login.this, signUp.class));
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/linkedinapp/signUp.java
+++ app/src/main/java/com/example/linkedinapp/signUp.java
@@ -1,13 +1,78 @@
 package com.example.linkedinapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class signUp extends AppCompatActivity {
+    private FirebaseAuth auth;
+    private EditText signUpname, signUpEmail, signUpPhone, sgnUpGender, signUpDescription, signUpSkill, signUpPassword, signUpConfPass;
+    private ImageView signUpimage;
+    private Button signUpButton;
+    private TextView loginText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        signUpButton = findViewById(R.id.signup_button);
+        signUpimage = findViewById(R.id.signUp_image);
+        signUpname = findViewById(R.id.signup_name);
+        signUpEmail = findViewById(R.id.signup_Email);
+        signUpPhone = findViewById(R.id.signup_Phonenumber);
+        sgnUpGender = findViewById(R.id.signup_gender);
+        signUpDescription = findViewById(R.id.signUp_Description);
+        signUpSkill = findViewById(R.id.signUp_skills);
+        signUpPassword = findViewById(R.id.signup_Password);
+        signUpConfPass = findViewById(R.id.signup_Confirm_Password);
+        loginText = findViewById(R.id.login_text);
+        auth = FirebaseAuth.getInstance();
+
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = signUpEmail.getText().toString().trim();
+                String password = signUpPassword.getText().toString().trim();
+
+                if (email.isEmpty()) {
+                    signUpEmail.setError("Email cannot be empty");
+                }
+                if (password.isEmpty()) {
+                    signUpPassword.setError("Password cannot be empty");
+                } else {
+                    auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(signUp.this, "Account Created Successfully", Toast.LENGTH_LONG).show();
+                                startActivity(new Intent(signUp.this, login.class));
+                            } else {
+                                Toast.makeText(signUp.this, "SignUp unsuccessful" + task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+        loginText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(signUp.this, login.class));
+            }
+        });
     }
 }
